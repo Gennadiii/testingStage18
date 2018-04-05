19ae0705e2f9ec684ddb4cd38c82740b8d04e1a0
Gennadii_Mishchevskii <gmishchevskii@cogniance.com>
2018-04-05 15:49:35 +0300

    Added numbers to logs in basic promise example

--- promises/05_promiseBasicExample.js
+++ promises/05_promiseBasicExample.js
@@ -1,26 +1,26 @@
 //latentflip.com/loupe/
 
-console.log('SYNC START');
+console.log('1 - SYNC START');
 
 setTimeout(function iAmTheFastest() {
-  console.log('I am the fastest async');
+  console.log('5 - I am the fastest async');
 }, 1000);
 
 function func1(myVar) {
   return new Promise(function (resolve, reject) {
-    console.log('Sync Promise start');
+    console.log('2 - Sync Promise start');
     setTimeout(function thisIsPromise() {
       myVar ? resolve(myVar) : reject(new Error('Rejected'));
     }, 10 * 1000);
-    console.log('Sync Promise end');
+    console.log('3 - Sync Promise end');
   });
 }
 
-func1("I'm from promise")
+func1("7 - I'm from promise")
   .then(console.log); // remove for loupe
 
 setTimeout(function asyncEnd() {
-  console.log('Async end');
+  console.log('6 - Async end');
 }, 2000);
 
-console.log('SYNC END');
+console.log('4 - SYNC END');
